76a43c9f7edadf606fb1f50896dea4ffe0f7a459
victorfdez <vicfernandezespana@gmail.com>
2022-05-22 17:45:09 +0200

    fix #1

--- src/com/vfe/eed/examen/OptimizacionYDocumentacion/EdadErroneaException.java
+++ src/com/vfe/eed/examen/OptimizacionYDocumentacion/EdadErroneaException.java
@@ -2,7 +2,7 @@ package com.vfe.eed.examen.OptimizacionYDocumentacion;
 
 public class EdadErroneaException extends Exception {
 
-	public EdadErroneaException(String string) {
-		super(string);
+	public EdadErroneaException(String message) {
+		super(message);
 	}
 }
