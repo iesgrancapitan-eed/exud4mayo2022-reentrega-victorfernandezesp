81a98b391a6fc541838790d5f6bb94661929962c
victorfdez <vicfernandezespana@gmail.com>
2022-05-22 18:33:25 +0200

    refactor: mensaje de la excepción

--- src/com/vfe/eed/examen/OptimizacionYDocumentacion/EdadErroneaException.java
+++ src/com/vfe/eed/examen/OptimizacionYDocumentacion/EdadErroneaException.java
@@ -2,7 +2,7 @@ package com.vfe.eed.examen.OptimizacionYDocumentacion;
 
 public class EdadErroneaException extends Exception {
 
-	public EdadErroneaException(String message) {
-		super(message);
+	public EdadErroneaException(String message2) {
+		super(message2);
 	}
 }
