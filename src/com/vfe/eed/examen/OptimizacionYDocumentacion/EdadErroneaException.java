54b1c11b8b2a29fe6760a4e4f28a436162f3c445
victorfdez <vicfernandezespana@gmail.com>
2022-05-22 17:32:28 +0200

    refactor: nombre del paquete

---
+++ src/com/vfe/eed/examen/OptimizacionYDocumentacion/EdadErroneaException.java
@@ -0,0 +1,8 @@
+package com.vfe.eed.examen.OptimizacionYDocumentacion;
+
+public class EdadErroneaException extends Exception {
+
+	public EdadErroneaException(String string) {
+		super(string);
+	}
+}
--- src/examenOptimizacionYDocumentacion/EdadErroneaException.java
+++
@@ -1,8 +0,0 @@
-package examenOptimizacionYDocumentacion;
-
-public class EdadErroneaException extends Exception {
-
-	public EdadErroneaException(String string) {
-		super(string);
-	}
-}
